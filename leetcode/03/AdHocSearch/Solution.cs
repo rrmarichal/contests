d04d5fec5c08f1d06b7256f74dfe937a0b3eaad4
Renier Marichal <rrmarichal@gmail.com>
2019-02-25 12:49:47 -0500

    Add leetcode 03

---
+++ leetcode/03/AdHocSearch/Solution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdHocSearch {
+
+    public class Solution {
+
+        public int LengthOfLongestSubstring(string s) {
+            var last = new Dictionary<char, int>();
+            var start = 0;
+            var max = 0;
+            for (var j = 0; j < s.Length; j++) {
+                var c = s[j];
+                if (!last.ContainsKey(c)) {
+                    last.Add(c, j);
+                }
+                else {
+                    start = Math.Max(start, last[c] + 1);
+                    last[c] = j;
+                }
+                if (j - start + 1 > max) {
+                    max = j - start + 1;
+                }
+            }
+            return max;
+        }
+
+    }
+
+}
