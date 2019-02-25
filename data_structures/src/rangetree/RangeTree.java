cd055416e354250d57adf34ce6d6cd60d40fd839
Renier Marichal <rrmarichal@gmail.com>
2019-02-25 11:51:57 -0500

    Add RangeTree stub

--- data_structures/src/rangetree/RangeTree.java
+++ data_structures/src/rangetree/RangeTree.java
@@ -1,5 +1,30 @@
 package rangetree;
 
 public class RangeTree {
+
+    private static final boolean CHECKED = true;
     
+    class Point {
+        public int x, y;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    public static RangeTree create(Point[] points) {
+        if (CHECKED && points == null) {
+            throw new IllegalArgumentException("Invalid argument (points is null).");
+        }
+        return new RangeTree(points);
+    }
+
+    private RangeTree(Point[] points) {
+        _buildTree(points);
+    }
+
+    private void _buildTree(Point[] points) {
+    }
+
 }
