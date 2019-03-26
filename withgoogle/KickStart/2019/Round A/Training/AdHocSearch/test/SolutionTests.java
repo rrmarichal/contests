ee99882ed43f85aec9235fb445d10d514f337a82
Renier Marichal <rrmarichal@gmail.com>
2019-03-26 16:04:44 -0400

    Add WithGoogle/KS/2019/A/Training test data.

--- withgoogle/KickStart/2019/Round A/Training/AdHocSearch/test/SolutionTests.java
+++ withgoogle/KickStart/2019/Round A/Training/AdHocSearch/test/SolutionTests.java
@@ -6,6 +6,15 @@ import org.junit.*;
 
 public class SolutionTests {
 
+    /**
+    3
+    4 3
+    3 1 9 100
+    6 2
+    5 5 1 2 3 4
+    5 5
+    7 7 1 7 7
+    */
     @Test
     public void test0() throws FileNotFoundException {
         InputReader in = new InputReader(new FileInputStream("test_data/0.in"));
