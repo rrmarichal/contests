d6e75151402f2f6db5dba9c939627241c2d8a7f7
Renier Marichal <rrmarichal@gmail.com>
2019-04-08 16:14:22 -0400

    Add withgoogle templates folder.

---
+++ withgoogle/templates/Java/src/Solution.java
@@ -0,0 +1,73 @@
+import java.io.*;
+import java.util.*;
+
+class InputReader {
+    private static final int BUFFER_SIZE = 1<<16;
+
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+
+    public InputReader(InputStream stream) {
+        reader = new BufferedReader(new InputStreamReader(stream), BUFFER_SIZE);
+        tokenizer = null;
+    }
+
+    public String next() {
+        while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+            try {
+                tokenizer = new StringTokenizer(reader.readLine());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return tokenizer.nextToken();
+    }
+
+    public int nextInt() {
+        return Integer.parseInt(next());
+    }
+
+    public long nextLong() {
+        return Long.parseLong(next());
+    }
+}
+
+class TestInfo {
+    public TestInfo() {
+
+    }
+}
+
+public class Solution {
+
+    private int T;
+    private TestInfo[] tests;
+
+    protected Solution(InputReader in) {
+        T = in.nextInt();
+        tests = new TestInfo[T];
+        for (int t = 0; t < T; t++) {
+            tests[t] = new TestInfo();
+        }
+    }
+
+    public String[] solve() {
+        String[] result = new String[T];
+        for (int t = 0; t < T; t++) {
+            result[t] = String.format("Case #%d: ?", t + 1);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        InputReader in = new InputReader(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        Solution solution = new Solution(in);
+        String[] result = solution.solve();
+        for (String c: result) {
+            out.println(c);
+        }
+        out.close();
+    }
+
+}
---
+++ withgoogle/templates/Java/test/SolutionTests.java
@@ -0,0 +1,19 @@
+import static org.junit.Assert.*;
+
+import java.io.*;
+
+import org.junit.*;
+
+public class SolutionTests {
+
+    @Test
+    public void test0() throws FileNotFoundException {
+        InputReader in = new InputReader(new FileInputStream("test_data/0.in"));
+        Solution solution = new Solution(in);
+        String[] result = solution.solve();
+        assertArrayEquals(new String[] {
+            "Case #1: ?"
+        }, result);
+    }
+
+}
