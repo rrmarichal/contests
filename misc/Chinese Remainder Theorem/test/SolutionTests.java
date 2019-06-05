d1c71e450934d75b7e51d7af3577155dc702f935
Renier Marichal <rrmarichal@gmail.com>
2019-06-05 11:05:22 -0400

    Add tests to EEA implementation.

--- misc/Chinese Remainder Theorem/test/SolutionTests.java
+++ misc/Chinese Remainder Theorem/test/SolutionTests.java
@@ -39,6 +39,78 @@ public class SolutionTests {
     }
 
     @Test
+    public void extendedEuclidTest3a() {
+        ExtendedEuclidResult r = Solution._extendedEuclid(23, 11);
+        assertEquals(1, r.d);
+        assertEquals(1, r.x);
+        assertEquals(-2, r.y);
+    }
+
+    @Test
+    public void extendedEuclidTest4() {
+        ExtendedEuclidResult r = Solution._extendedEuclid(28, -11);
+        assertEquals(1, r.d);
+        assertEquals(2, r.x);
+        assertEquals(5, r.y);
+    }
+
+    @Test
+    public void extendedEuclidTest4a() {
+        ExtendedEuclidResult r = Solution._extendedEuclid(62, -23);
+        assertEquals(-1, r.d);
+        assertEquals(10, r.x);
+        assertEquals(27, r.y);
+    }
+
+    @Test
+    public void extendedEuclidTest6() {
+        ExtendedEuclidResult r = Solution._extendedEuclid(28*23 + 11*62, -2*11*23);
+        assertEquals(-2, r.d);
+        assertEquals(29, r.x);
+        assertEquals(76, r.y);
+    }
+
+    @Test
+    public void extendedEuclidTest7() {
+        ExtendedEuclidResult r = Solution._extendedEuclid(28*23 + 11*62, -2*11*23);
+        assertEquals(-2, r.d);
+        assertEquals(29, r.x);
+        assertEquals(76, r.y);
+    }
+
+    @Test
+    public void extendedEuclidTest8() {
+        ExtendedEuclidResult r = Solution._extendedEuclid(28 + 11, 62 + 23);
+        assertEquals(1, r.d);
+        assertEquals(24, r.x);
+        assertEquals(-11, r.y);
+    }
+
+    @Test
+    public void extendedEuclidTest9() {
+        ExtendedEuclidResult r = Solution._extendedEuclid(28 + 62, 11 + 23);
+        assertEquals(2, r.d);
+        assertEquals(-3, r.x);
+        assertEquals(8, r.y);
+    }
+
+    @Test
+    public void extendedEuclidTesta() {
+        ExtendedEuclidResult r = Solution._extendedEuclid(694166449 + 679334523, 402616793 + 389956898);
+        assertEquals(1, r.d);
+        assertEquals(-376220157, r.x);
+        assertEquals(651975655, r.y);
+    }
+
+    @Test
+    public void extendedEuclidTestb() {
+        ExtendedEuclidResult r = Solution._extendedEuclid(743506299 + 694166449, 476597252 + 402616793);
+        assertEquals(1, r.d);
+        assertEquals(-141856188, r.x);
+        assertEquals(231960325, r.y);
+    }
+
+    @Test
     public void test0() throws FileNotFoundException {
         InputReader in = new InputReader(new FileInputStream("test_data/0.in"));
         Solution solution = new Solution(in);
