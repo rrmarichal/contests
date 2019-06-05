aad0b6913281ecaff54ea0d15e46a5e7ed897e41
Renier Marichal <rrmarichal@gmail.com>
2019-06-05 11:04:30 -0400

    Add new implementation for InputReader to Java templates.

--- withgoogle/templates/Java/src/Solution.java
+++ withgoogle/templates/Java/src/Solution.java
@@ -4,31 +4,63 @@ import java.util.*;
 class InputReader {
     private static final int BUFFER_SIZE = 1<<16;
 
-    public BufferedReader reader;
-    public StringTokenizer tokenizer;
+    private BufferedReader reader;
+    private StringTokenizer tokenizer;
 
     public InputReader(InputStream stream) {
         reader = new BufferedReader(new InputStreamReader(stream), BUFFER_SIZE);
-        tokenizer = null;
     }
 
-    public String next() {
-        while (tokenizer == null || !tokenizer.hasMoreTokens()) {
-            try {
-                tokenizer = new StringTokenizer(reader.readLine());
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+    private String readLine() {
+        try {
+            return reader.readLine();
+        }
+        catch (IOException e) {
+            return null;
+        }
+    }
+
+    private boolean ensureTokenizer() {
+        if (tokenizer == null || !tokenizer.hasMoreTokens()) {
+            String line = readLine();
+            if (line != null) {
+                tokenizer = new StringTokenizer(line);
             }
         }
-        return tokenizer.nextToken();
+        return tokenizer != null && tokenizer.hasMoreElements();
     }
 
-    public int nextInt() {
-        return Integer.parseInt(next());
+    public String nextLine() {
+        if (ensureTokenizer()) {
+            StringBuilder result = new StringBuilder();
+            while (tokenizer.hasMoreTokens()) {
+                result.append(tokenizer.nextToken());
+                result.append(' ');
+            }
+            return result.toString();
+        }
+        return null;
     }
 
-    public long nextLong() {
-        return Long.parseLong(next());
+    public String nextToken() {
+        if (ensureTokenizer()) {
+            return tokenizer.nextToken();
+        }
+        return null;
+    }
+
+    public Integer nextInt() {
+        if (ensureTokenizer()) {
+            return Integer.parseInt(tokenizer.nextToken());
+        }
+        return null;
+    }
+
+    public Long nextLong() {
+        if (ensureTokenizer()) {
+            return Long.parseLong(tokenizer.nextToken());
+        }
+        return null;
     }
 }
 
