86fdc79d1831423c71cb1c9c2a9b71d65a2863e4
Renier Marichal <rrmarichal@gmail.com>
2025-11-27 11:28:28 -0600

    Add Felix Pago interview

---
+++ hackerrank/bear-steady-gene/Program.cs
@@ -0,0 +1,139 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class GeneCount {
+    public GeneCount(int c, int g, int a, int t) {
+        this.C = c;
+        this.G = g;
+        this.A = a;
+        this.T = t;
+    }
+
+    public int C { get; }
+    public int G { get; }
+    public int A { get; }
+    public int T { get; }
+
+    public GeneCount withGene(char value) {
+        if (value == 'C') {
+            return new GeneCount(C+1, G, A, T);
+        }
+
+        if (value == 'G') {
+            return new GeneCount(C, G+1, A, T);
+        }
+
+        if (value == 'A') {
+            return new GeneCount(C, G, A+1, T);
+        }
+
+        // Default to T
+        return new GeneCount(C, G, A, T+1);
+    }
+
+    public GeneCount add(GeneCount value) {
+        return new GeneCount(C + value.C, G + value.G, A + value.A, T + value.T);
+    }
+
+    public GeneCount subtract(GeneCount value) {
+        return new GeneCount(C - value.C, G - value.G, A - value.A, T - value.T);
+    }
+
+    public override string ToString() {
+        return $"C:{C} G:{G} A:{A} T:{T}";
+    }
+}
+
+class Result {
+    /*
+     * Complete the 'steadyGene' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING gene as parameter.
+     */
+
+    public static int steadyGene(string gene) {
+        var l = 0;
+        var r = gene.Length - 1;
+        var memo = precomp(gene);
+
+        while (l < r) {
+            var target = l + (r - l)/2;
+            if (steady(gene, memo, target)) {
+                r = target;
+            } else {
+                l = target + 1;
+            }
+        }
+        return l;
+    }
+
+    private static GeneCount[] precomp(string gene) {
+        var result = new GeneCount[gene.Length];
+        result[0] = new GeneCount(0, 0, 0, 0).withGene(gene[0]);
+        for (var i = 1; i < gene.Length; i++) {
+            result[i] = result[i - 1].withGene(gene[i]);
+        }
+
+        return result;
+    }
+
+    private static bool steady(string gene, GeneCount[] memo, int count) {
+        var threshold = gene.Length / 4;
+        for (var i = 0; i < gene.Length - count + 1; i++) {
+            // test replacing sequence gene[i..i+target] balances gene count at or below threshold.
+            var geneCount = countMemo(gene, memo, i, count);
+            if (geneCount.C > threshold
+                || geneCount.G > threshold
+                || geneCount.A > threshold
+                || geneCount.T > threshold) {
+                continue;
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+
+    private static GeneCount countMemo(string gene, GeneCount[] memo, int index, int count) {
+        // Count occurrences of value in [0..index-1] + [index+count..gene.Length - 1]
+        var result = index > 0 ? memo[index - 1] : new GeneCount(0, 0, 0, 0);
+        if (index + count < gene.Length) {
+            result = result.add(memo[gene.Length - 1]);
+            result = result.subtract(memo[index + count - 1]);
+        }
+
+        return result;
+    }
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(Console.OpenStandardOutput());
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        string gene = Console.ReadLine();
+
+        int result = Result.steadyGene(gene);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
---
+++ hackerrank/felix-pago/Program.cs
@@ -0,0 +1,183 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class MaxHeapComparer : IComparer<int>
+{
+    public int Compare(int x, int y)
+    {
+        // For a max heap, we want larger values to have higher priority,
+        // so we reverse the default comparison order.
+        return y.CompareTo(x);
+    }
+}
+
+class PairComparer : IComparer<Pair>
+{
+    public int Compare(Pair x, Pair y)
+    {
+        return y.Value.CompareTo(x.Value);
+    }
+}
+
+class Pair {
+    public int Value;
+    public int Index;
+}
+
+class Result
+{
+
+    /*
+     * Complete the 'frequencyOfMaxNewUsers' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY numbers
+     *  2. INTEGER_ARRAY q
+     */
+
+    public static List<int> frequencyOfMaxNewUsers(List<int> numbers, List<int> q) {
+        // return Optimal(numbers, q);
+        // return BruteForce(numbers, q);
+        return BruteForce2(numbers, q);
+    }
+
+    private static List<int> Optimal2(List<int> numbers, List<int> q) {
+        var pairs = new List<Pair>();
+        for (var i = 0; i < numbers.Count; i++) {
+            pairs.Add(new Pair { Value = numbers[i], Index = i + 1 });
+        }
+
+        pairs.Sort(new PairComparer());
+        // [(5,1), (5,3), (4,2), (3,4), (2,5)]
+
+
+        return numbers;
+    }
+
+    private static List<int> BruteForce2(List<int> numbers, List<int> q) {
+        // var result = new List<int>();
+        var max = numbers.Last();
+        var count = 1;
+        var queryMap = new Dictionary<int, int>();
+        queryMap.Add(numbers.Count, count);
+
+        for (var index = numbers.Count - 1; index > 0; index--) {
+            if (numbers[index] > max) {
+                max = numbers[index];
+                count = 1;
+            }
+            else if (numbers[index] == max) {
+                count++;
+            }
+
+            queryMap.Add(index, count);
+         }
+
+         var result = new List<int>();
+         for (var i = 0; i < q.Count; i++) {
+            result.Add(queryMap[q[i]]);
+         }
+
+         return result;
+    }
+
+    private static List<int> BruteForce(List<int> numbers, List<int> q) {
+        var result = new List<int>();
+
+        for (var i = 0; i < q.Count; i++) {
+            result.Add(FindMaxCount(numbers, q[i]-1));
+        }
+
+        return result;
+    }
+
+    private static int FindMaxCount(List<int> numbers, int index) {
+        var max = numbers[index];
+        var count = 1;
+
+        for (var i = index+1; i < numbers.Count; i++) {
+            if (numbers[i] == max) {
+                count++;
+            }
+            else if (numbers[i] > max) {
+                max = numbers[i];
+                count = 1;
+            }
+        }
+
+        return count;
+    }
+
+    private static List<int> Optimal(List<int> numbers, List<int> q) {
+        var map = new Dictionary<int, int>();
+        for (var i = 0; i < numbers.Count; i++) {
+            if (!map.ContainsKey(numbers[i])) {
+                map.Add(numbers[i], 0);
+            }
+
+            map[numbers[i]]++;
+        }
+
+        var maxHeap = new PriorityQueue<int, int>(numbers.Count, new MaxHeapComparer());
+        foreach (var value in numbers) {
+            maxHeap.Enqueue(value, value);
+        }
+
+        // q.Sort();
+
+        for (var i = 0; i < q.Count; i++) {
+            var max = maxHeap.Peek();
+            // maxHeap.Dequeue();
+        }
+
+        return new List<int>();
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(new Console.OpenStandardOutput());
+
+        int numbersCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> numbers = new List<int>();
+
+        for (int i = 0; i < numbersCount; i++)
+        {
+            int numbersItem = Convert.ToInt32(Console.ReadLine().Trim());
+            numbers.Add(numbersItem);
+        }
+
+        int qCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> q = new List<int>();
+
+        for (int i = 0; i < qCount; i++)
+        {
+            int qItem = Convert.ToInt32(Console.ReadLine().Trim());
+            q.Add(qItem);
+        }
+
+        List<int> result = Result.frequencyOfMaxNewUsers(numbers, q);
+
+        textWriter.WriteLine(String.Join("\n", result));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
