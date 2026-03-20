62c2e630c2ee050688c786b5b3d97fd97b5f53ba
Renier Marichal <rrmarichal@gmail.com>
2026-03-19 19:07:31 -0600

    Codeforces 823 Problem C

---
+++ codeforces/round823/C-CountInversions/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Text;
+
+class Program
+{
+  static string _MergeSort(string s, int l, int h)
+  {
+    if (l == h) return s[l].ToString();
+
+    var pivot = (l + h)/2;
+    var ls = _MergeSort(s, l, pivot);
+    var rs = _MergeSort(s, pivot+1, h);
+
+    var inversions = 0;
+    var li = 0;
+    var ri = 0;
+    var result = new StringBuilder();
+    while (li < ls.Length || ri < rs.Length)
+    {
+      if (li == ls.Length)
+      {
+        result.Append(rs[ri++]);
+        continue;
+      }
+      if (ri == rs.Length)
+      {
+        var shift = Math.Min(ls[li++] + inversions, '9');
+        result.Append((char) shift);
+        continue;
+      }
+
+      if (ls[li] + inversions <= rs[ri])
+      {
+        var shift = Math.Min(ls[li++] + inversions, '9');
+        result.Append((char) shift);
+        li++;
+      }
+      else
+      {
+        result.Append(rs[ri++]);
+        inversions++;
+      }
+    }
+
+    return result.ToString();
+  }
+
+  static string Solve(string s)
+  {
+    return _MergeSort(s, 0, s.Length - 1);
+  }
+
+  public static void Main(string[] args)
+  {
+    var N = int.Parse(Console.ReadLine());
+    for (var i = 0; i < N; i++)
+    {
+      Console.WriteLine(Solve(Console.ReadLine()));
+    }
+  }
+}
---
+++ codeforces/round823/C-Greedy/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+
+class Program
+{
+  static string Solve(string s)
+  {
+    var lastIndex = new int[10];
+    var indices = new List<int>[10];
+    for (var i = 0; i < s.Length; i++)
+    {
+      var key = s[i]-'0';
+      lastIndex[key] = i;
+      if (indices[key] == null)
+      {
+        indices[key] = [];
+      }
+      indices[key].Add(i);
+    }
+
+    var result = new StringBuilder();
+    for (var c = 0; c <= 9; c++)
+    {
+      if (indices[c] == null) continue;
+
+      for (var i = indices[c].Count - 1; i >= 0; i--)
+      {
+        var index = indices[c][i];
+        var less = false;
+        for (var k = 0; k < c; k++)
+        {
+          if (lastIndex[k] > index)
+          {
+            less = true;
+            break;
+          }
+        }
+
+        result.Append(less ? Math.Min(c+1, 9).ToString() : c.ToString());
+      }
+    }
+
+    return result.ToString();
+  }
+
+  public static void Main(string[] args)
+  {
+    var N = int.Parse(Console.ReadLine());
+    for (var i = 0; i < N; i++)
+    {
+      Console.WriteLine(Solve(Console.ReadLine()));
+    }
+  }
+}
